c2e5275d907c8e7d062ea11d8f7527b70fdc55ec
lanahajhussain <lanahajhussain@yahoo.com>
2020-05-02 19:50:02 +0400

    hhkhj

--- Secuirty/sec4.py
+++ Secuirty/sec4.py
@@ -258,10 +258,12 @@ from keras.layers import Flatten
 def create_modelCNN():
    
     model = Sequential()
-    model.add(Conv1D(filters=2, kernel_size=1, activation='relu', input_shape=(1,18)))
+    model.add(Conv1D(filters=5, kernel_size=1, activation='relu', input_shape=(1,18)))
     model.add(Dropout(0.5))
+    model.add(MaxPooling1D(pool_size=2))
+
     model.add(Flatten())
-    model.add(Dense(54, activation='relu'))
+    model.add(Dense(90, activation='relu'))
     model.add(Dense(4, activation='softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
