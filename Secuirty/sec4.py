b78b5fb4f7ab8d366201fadc47881dbb0ed9edc9
lanahajhussain <lanahajhussain@yahoo.com>
2020-05-02 18:49:39 +0400

    hhkhj

--- Secuirty/sec4.py
+++ Secuirty/sec4.py
@@ -258,7 +258,7 @@ from keras.layers import Flatten
 def create_modelCNN():
     X_new.reshape(80000,1,18)
     model = Sequential()
-    model.add(Conv1D(filters=2, kernel_size=3, activation='relu', input_shape=(100,18)))
+    model.add(Conv1D(filters=2, kernel_size=3, activation='relu', input_shape=(1,18)))
     model.add(Dropout(0.5))
     model.add(MaxPooling1D(pool_size=2))
     model.add(Flatten())
