c19c7f10f8698b398f33d1671c089c11904ce7b3
lanahajhussain <lanahajhussain@yahoo.com>
2020-05-02 18:51:28 +0400

    hhkhj

--- Secuirty/sec4.py
+++ Secuirty/sec4.py
@@ -260,8 +260,6 @@ def create_modelCNN():
     model = Sequential()
     model.add(Conv1D(filters=2, kernel_size=1, activation='relu', input_shape=(1,18)))
     model.add(Dropout(0.5))
-    model.add(MaxPooling1D(pool_size=2))
-    model.add(Flatten())
     model.add(Dense(100, activation='relu'))
     model.add(Dense(4, activation='softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
